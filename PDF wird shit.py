edc5b34ec6767081b7aa1475af7510ae3cccdf07
mammaJustKillAMan <nata.jalosinska@gmail.com>
2025-11-03 17:53:12 +0100

    menu added

--- PDF wird shit.py
+++ PDF wird shit.py
@@ -73,3 +73,33 @@ def add_text(pdf_path, output_path, phrase, page_number=0, x=50, y=50):
     print(f"Text added to PDF and saved to {output_path}")
 
 #here was supposed to be a main with menu but i have no more strength rn, still ill
+if __name__ == "__main__":
+    print("___PDF FUNCTIONS MENU___")
+    print("Please, choose carefully, for wrongful usage the programmer does not take credit")
+    print("1. Count pages")
+    print("2. Extract text")
+    print("3. Count images")
+    print("4. Count words")
+    print("5. Add text")
+    print("6. Exit")
+
+    answer = input("Enter your choice: ")
+    if answer == "6":
+        exit()
+
+    pdf_path = input("Enter path to PDF: ")
+    output_path = input("Enter path to output PDF: ")
+    if answer == "1":
+        print("Page count:", count_pages(pdf_path))
+    elif answer == "2":
+        extract_text(pdf_path)
+    elif answer == "3":
+        count_images(pdf_path)
+    elif answer == "4":
+        word = input("Enter word to count: ")
+        count_words(pdf_path, word)
+    elif answer == "5":
+        phrase = input("Enter phrase to add: ")
+        add_text(pdf_path, output_path, phrase)
+    else:
+        print("Invalid choice.")
